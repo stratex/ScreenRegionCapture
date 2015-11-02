5025781f5231af99528df10e555ac6b5bf0b4a60
stratex5 <aqja88@hotmail.com>
2015-11-03 07:37:13 +1300

    Created seperate class Added events TODO -Add example -return region

---
+++ ScreenRegionCapture/ScreenRegionCaptureGUI/Classes/ScreenRegionCapture.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ScreenRegionCaptureGUI.Classes
+{
+    class ScreenRegionCapture
+    {
+        #region GlobalVariables
+        private int Height { get; set; }
+        private int Width { get; set; }
+        private int blockSize { get; set; }
+        private Bitmap currentBitmap { get; set; }
+        System.Threading.Timer screenBufferTimer { get; set; }
+        #endregion
+
+        #region Delegates and Events
+        public delegate void OnRegionUpdatedCallback(int xBlock, int yBlock);
+        public event OnRegionUpdatedCallback OnRegionUpdated;
+        #endregion
+
+        #region Constructors
+        public ScreenRegionCapture()
+        {
+            Height = Screen.PrimaryScreen.Bounds.Height;
+            Width = Screen.PrimaryScreen.Bounds.Width;
+            currentBitmap = GetScreenShot();
+            blockSize = 64;
+        }
+
+        public ScreenRegionCapture(int block)
+            : this()
+        {
+            blockSize = block;
+        }
+        #endregion
+
+        #region Methods
+        public bool Start(int interval)
+        {
+            try
+            {
+                TimerCallback OnTimerTick = new TimerCallback(Tick);
+                screenBufferTimer = new System.Threading.Timer(OnTimerTick, null, interval, interval);
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+        public bool Stop()
+        {
+            try
+            {
+                screenBufferTimer.Dispose();
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+        private Bitmap GetScreenShot()
+        {
+            var bmpScreenshot = new Bitmap(Screen.PrimaryScreen.Bounds.Width,
+                Screen.PrimaryScreen.Bounds.Height,
+                PixelFormat.Format32bppArgb);
+
+            var gfxScreenshot = Graphics.FromImage(bmpScreenshot);
+
+            gfxScreenshot.CopyFromScreen(Screen.PrimaryScreen.Bounds.X,
+                Screen.PrimaryScreen.Bounds.Y,
+                0,
+                0,
+                Screen.PrimaryScreen.Bounds.Size,
+                CopyPixelOperation.SourceCopy);
+
+            return bmpScreenshot;
+        }
+
+        private void CompareBitmap(Bitmap nextFrame)
+        {
+            for (var yBlock = 0; yBlock < Height / blockSize; yBlock++)
+                for (var xBlock = 0; xBlock < Width / blockSize; xBlock++)
+                    for (var y = yBlock * blockSize; y < yBlock * blockSize + blockSize; y++)
+                        for (var x = xBlock * blockSize; x < xBlock * blockSize + blockSize; x++)
+                            if (currentBitmap.GetPixel(x, y) != nextFrame.GetPixel(x, y))
+                            {
+                                if (OnRegionUpdated != null)
+                                    OnRegionUpdated(xBlock, yBlock);
+                                break;
+                            }
+        }
+        #endregion
+
+        #region Callbacks
+        private void Tick(object state)
+        {
+            CompareBitmap(GetScreenShot());
+        }
+        #endregion
+    }
+}
--- ScreenRegionCapture/ScreenRegionCaptureGUI/MainForm.cs
+++ ScreenRegionCapture/ScreenRegionCaptureGUI/MainForm.cs
@@ -9,73 +9,9 @@ namespace ScreenRegionCaptureGUI
 {
     public partial class MainForm : Form
     {
-        #region Variables
-
-        private int ScreenHeight;
-        private int ScreenWidth;
-        private int blockSize = 64;
-
-        #endregion
-
         public MainForm()
         {
-            InitializeComponent();
-
-            ScreenHeight = Screen.FromControl(this).Bounds.Height;
-            ScreenWidth = Screen.FromControl(this).Bounds.Width;
-
-            while (true)
-            {
-                Thread.Sleep(1000);
-                CheckScreenUpdate();
-            }
-
-            //var onTick = new TimerCallback(DrawInitBounds);
-            //var drawBoundsTimer = new System.Threading.Timer(onTick, null, 0, 1000);
-
-        }
-
-        private Bitmap currentScreen;
-
-        private void CheckScreenUpdate()
-        {
-            //Vars
-            string currentVal;
-            string NextVal;
-
-            #region ScreenShot
-
-            // Get Screen
-            var bmpScreenshot = new Bitmap(Screen.PrimaryScreen.Bounds.Width,
-                Screen.PrimaryScreen.Bounds.Height,
-                PixelFormat.Format32bppArgb);
-
-            var gfxScreenshot = Graphics.FromImage(bmpScreenshot);
-
-            gfxScreenshot.CopyFromScreen(Screen.PrimaryScreen.Bounds.X,
-                Screen.PrimaryScreen.Bounds.Y,
-                0,
-                0,
-                Screen.PrimaryScreen.Bounds.Size,
-                CopyPixelOperation.SourceCopy);
-
-            #endregion
-
-            if (currentScreen == null)
-            {
-                currentScreen = bmpScreenshot;
-                return;
-            }
-            else
-                for (var yBlock = 0; yBlock < ScreenHeight/blockSize; yBlock++)
-                    for (var xBlock = 0; xBlock < ScreenWidth/blockSize; xBlock++)
-                        for (var y = yBlock * blockSize; y < yBlock * blockSize + blockSize; y++)
-                            for (var x = xBlock * blockSize; x < xBlock * blockSize + blockSize; x++)
-                                if (currentScreen.GetPixel(x, y) != bmpScreenshot.GetPixel(x, y))
-                                {
-                                    Console.WriteLine("REGION CHANGED: {0}:{1}", xBlock, yBlock);
-                                    break;
-                                }
+            
         }
     }
 }
